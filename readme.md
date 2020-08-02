69f15974c308871c7115927fa3cc363cec0813a6
ksprivate <kemal.selimbasic.2016@gmail.com>
2020-08-02 13:35:54 +0200

    seting up

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 initial commit-
+# rtc-chat
---
+++ script.js
@@ -0,0 +1,24 @@
+let btn=document.getElementById('submit-btn');
+btn.addEventListener('click',function(){
+printMsg()
+})
+
+function printMsg(msg,args){
+let main=document.getElementById('main');
+for(let i=0;i<20;i++){
+let message=document.createElement('div');
+message.classList.add('message');
+if(i%3==0){
+    message.classList.add(args);
+
+}
+else{
+    message.classList.add('recived');
+
+}
+message.innerHTML=msg
+main.append(message)
+}
+};
+
+
