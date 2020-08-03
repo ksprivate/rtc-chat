c087083aa2dfd9cbb5cfcebf344904865ad0db8a
ksprivate <kemal.selimbasic.2016@gmail.com>
2020-08-03 23:13:45 +0200

    sending messages finished

--- public/script.js
+++ public/script.js
@@ -1,32 +1,45 @@
-const socket = io('http://127.0.0.1:3000');
+const messagenput = document.getElementById("message");
+const socket = io("http://127.0.0.1:3000");
+let name = null;
+window.onload = function () {
+  name = prompt("Enter nickname");
 
-socket.on('message',data=>{
-    console.log(data)
-})
-
-
-
-let btn=document.getElementById('submit-btn');
-btn.addEventListener('click',function(){
-printMsg()
-})
-
-function printMsg(msg,args){
-let main=document.getElementById('main');
-for(let i=0;i<20;i++){
-let message=document.createElement('div');
-message.classList.add('message');
-if(i%3==0){
-    message.classList.add(args);
-
-}
-else{
-    message.classList.add('recived');
-
-}
-message.innerHTML=msg
-main.append(message)
-}
+  socket.emit("user-connected", id, name);
+  printMsg("You joined", "sent");
 };
+window.onunload = function () {
+  socket.emit("user-disconnected", id, name);
+};
+socket.on("user-disconnected", (data) => {
+  printMsg(`${data} left!`, "recived");
+});
+socket.on("user-connected", (data) => {
+  printMsg(`${data} joined!`, "recived");
+});
+socket.on("message", (data) => {
+  printMsg(data, "recived");
+});
+const form = document.getElementById("send-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  let messagetext = messagenput.value;
 
+  printMsg(messagetext, "sent");
+
+  socket.emit("message", id, messagetext);
+
+  messagenput.value = "";
+});
+
+function printMsg(msg, args) {
+  let main = document.getElementById("main");
+
+  let message = document.createElement("div");
+  message.classList.add("message");
+
+  message.classList.add(args);
+
+  message.innerHTML = msg;
+  main.append(message);
+}
--- server.js
+++ server.js
@@ -1,41 +1,68 @@
-require('dotenv').config()
-const express=require('express')
-const app =express()
-const server=require('http').Server(app)
-const io =require('socket.io')(server)
-const {nanoid}=require('nanoid')
-const mongo=require('mongodb')
-const message=require('./models/message')
-app.set('view engine','ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { nanoid } = require("nanoid");
+const mongo = require("mongodb");
+const message = require("./models/message");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+const rooms = [];
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.post("/createnewroom", (req, res) => {
+  let id = nanoid();
 
-app.get('/',(req,res)=>{
+  let room = { roomid: id, users: [] };
+  rooms.push(room);
+  res.redirect("/" + id);
+});
 
-res.render('home')
- 
-   res.redirect('/'+id.nanoid())
-})
-app.get('/createnewroom',(req,res)=>{
-    let id=nanoid()
-    mongo.MongoClient.connect(process.env.DATABASE,{useUnifiedTopology:true},(err,db)=>{
-        db.db('chat').collection(id).insertOne({active:true},(err,response)=>{
-            if(err) throw err
-        })
-        
-    })
-    res.redirect('/'+id)
-})
-
-
-app.get('/:id',(req,res)=>{
-    res.render('message')
-    io.on('connection',socket=>{
-    socket.join(req.params.id)
-})
-
-io.to(req.params.id).emit('message',"message text");
+app.get("/:id", (req, res) => {
     
-})
+    rooms.forEach(o=>{
+        if(o.roomid===req.params.id){
+      
+            res.render("message", {
+                id: o.roomid,
+                users:o.users 
+              });
+        }
+        else{
+            res.status(404)
+            res.send('room not found')
+            res.end()
+        }
+    })
+  
+});
+io.on("connection", (socket) => {
+  socket.on("message", (id, data) => {
+    socket.join(id)
+    socket.to(id).broadcast.emit("message", data);
+  });
+  socket.on("user-connected", (id, data) => {
+    socket.join(id)
+   rooms.forEach((o) => {
+       if(o.roomid===id){
+       o.users.push(data)
+   }})
+    socket.to(id).broadcast.emit("user-connected", data);
+  });
+  socket.on("user-disconnected", (id, data) => {
+    socket.join(id)
 
-server.listen(process.env.PORT||3000)
+    socket.to(id).broadcast.emit("user-disconnected", data);
+  });
+});
+
+/*io.on('connection',socket=>{
+    socket.on('message2',(id,data)=>{
+        console.log(data)
+        console.log(id)
+    })
+})*/
+server.listen(process.env.PORT || 3000);
